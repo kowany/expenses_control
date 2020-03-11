a8fba4be332d64d3bb4ec757cf701829d6dc3c86
kowany <hvilla65@gmail.com>
2020-03-10 21:24:11 -0600

    Modificado archivo main y creado carpeta pages, conteniendo HomePage y AddPage

---
+++ lib/pages/add_page.dart
@@ -0,0 +1,225 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../category_selection_widget.dart';
+
+
+class AddPage extends StatefulWidget {
+  @override
+  _AddPageState createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+
+  String category;
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text(
+          'Category',
+          style: TextStyle(
+            color: Colors.grey
+          ),
+        ),
+        centerTitle: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.close,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            }
+          )
+        ],
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body () {
+    return Column(
+      children: <Widget>[
+        _categorySelector(),
+        _currentValue(),
+        _numpad(),
+        _submit()
+      ],
+    );
+  }
+
+  Widget _categorySelector() {
+    return Container(
+      height: 80.0,
+      child: CategorySelectionWidget(
+        categories: {
+          'Shopping': Icons.shopping_cart,
+          'Alcohol': FontAwesomeIcons.beer,
+          'Fast food': FontAwesomeIcons.hamburger,
+          'Bills': FontAwesomeIcons.wallet
+        },
+        onValueChanged: ( newCategory ) => category = newCategory
+      )
+    );
+  }
+
+  Widget _currentValue() {
+    var realValue = value / 100.0;
+    return Padding(
+      padding: const EdgeInsets.symmetric( vertical: 18.0),
+      child: Text(
+        '\$${realValue.toStringAsFixed( 2 )}',
+        style: TextStyle(
+          fontSize: 50.0,
+          color: Colors.blueAccent,
+          fontWeight: FontWeight.w500
+        ),
+      ),
+    );
+  }
+
+  Widget _num ( String text, double height ) {
+    return GestureDetector(
+      // permite que al hacer click sobre cualquier espacio del
+      // contenedor del número, sea lo mismo que al hacerlo sobre
+      // el propio número
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        setState(() {
+          if ( text == ',' ) {
+            value = value * 100;
+          } else {
+            value = value * 10 + int.parse( text );
+          }
+        });
+      },
+      child: Container(
+        height: height,
+        child: Center(
+          child: Text( 
+            text,
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.grey
+            ),
+          ),
+        )
+      ),
+    );
+  }
+
+  Widget _numpad() {
+    return Expanded(
+      child: LayoutBuilder(
+        builder: ( BuildContext context, BoxConstraints constrainsts) {
+          var height = constrainsts.biggest.height / 4.0;
+          return Table(
+            border: TableBorder.all(
+              color: Colors.grey,
+              width: 1.0
+            ),
+            children: [
+              TableRow(
+                children: [
+                  _num( '1', height ),
+                  _num( '2', height ),
+                  _num( '3', height ),
+                ]
+              ),
+              TableRow(
+                children: [
+                  _num( '4', height ),
+                  _num( '5', height ),
+                  _num( '6', height ),
+                ]
+              ),
+              TableRow(
+                children: [
+                  _num( '7', height ),
+                  _num( '8', height ),
+                  _num( '9', height ),
+                ]
+              ),
+              TableRow(
+                children: [
+                  _num( ',', height ),
+                  _num( '0', height ),
+                  GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () {
+                      setState(() {
+                        value = value ~/ 10;
+                      });
+                    },
+                    child: Container(
+                      height: height,
+                      child: Center(
+                        child: Icon(
+                          Icons.backspace,
+                          color: Colors.grey,
+                          size: 40.0,
+                        ),
+                      ),
+                    ),
+                  )
+                ]
+              ),
+            ],
+          );
+        },
+      )
+    );
+  }
+  Widget _submit( ) {
+    return Builder(
+      builder: ( BuildContext context ) {
+        return Container(
+          height: 50.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.blueAccent
+          ),
+          child: MaterialButton(
+            child: Text( 
+              'Add expense',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.w500
+              ),
+            ),
+            onPressed: () {
+              if ( value > 0 && category != '' ) {
+                Firestore.instance
+                  .collection( 'expenses' )
+                  .document()
+                  .setData({
+                    'category': category,
+                    'value': value,
+                    'month': DateTime.now().month,
+                    'day': DateTime.now().day
+                  });
+                  Navigator.of( context ).pop();
+              } else {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar( content: Text( 'Enter a value and select a category' ) )
+                  );
+
+                }
+
+              
+            }
+          )
+        );
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:expenses_control_app/month_widget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  PageController _controller;
+  int curremtPage = 2;
+  Stream< QuerySnapshot > _query;
+
+
+  @override
+  void initState() { 
+    super.initState();
+    
+    _query = Firestore.instance
+              .collection('expenses')
+              .where("month", isEqualTo: curremtPage + 1 )
+              .snapshots();
+    
+    _controller = PageController(
+      initialPage: curremtPage,
+      viewportFraction: 0.3
+    );
+  }
+
+
+  Widget _bottomAction( IconData icon ) {
+    // el widget InkWell permite que al presionar un botón haga el efecto de onda
+    return InkWell(
+      child: Icon( icon ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8.0,
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _bottomAction( FontAwesomeIcons.history ),
+            _bottomAction( FontAwesomeIcons.chartPie ),
+            SizedBox( width: 48.0 ),
+            _bottomAction( FontAwesomeIcons.wallet ),
+            _bottomAction( Icons.settings ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.add ),
+        onPressed: () {
+          Navigator.of( context ).pushNamed( '/add' );
+        }
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget> [
+          _selector(),
+          StreamBuilder<QuerySnapshot>(
+            stream: _query,
+            builder: ( BuildContext context, AsyncSnapshot < QuerySnapshot > data ) {
+
+              if ( data.connectionState == ConnectionState.waiting ) {
+                return Center(
+                  child: CircularProgressIndicator()
+                );
+              } else {
+                return MonthWidget(
+                  documents: data.data.documents 
+                );
+              }
+
+            }
+          )
+        ]
+      )
+    );
+  }
+
+  Widget _pageItem( String name, int position ) {
+    var _alignment;
+
+     final selected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey,
+    );
+    final unselected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.blueGrey.withOpacity(0.4),
+    );
+
+    if ( position == curremtPage ) {
+      _alignment = Alignment.center;
+    } else if ( position > curremtPage ) {
+      _alignment = Alignment.centerRight;
+    } else {
+      _alignment = Alignment.centerLeft;
+    }
+
+
+    return Align(
+      alignment: _alignment,
+      child: Text( 
+        name,
+        style: position == curremtPage ? selected : unselected,
+       )
+    );
+  }
+
+  Widget _selector() {
+    return SizedBox.fromSize(
+      size: Size.fromHeight( 70.0 ),
+      child: PageView(
+        onPageChanged: ( newPage ) {
+          setState(() {
+            curremtPage = newPage;
+            _query = Firestore.instance
+                      .collection('expenses')
+                      .where("month", isEqualTo: curremtPage + 1 )
+                      .snapshots();
+          });
+        },
+        controller: _controller,
+        children: <Widget>[
+            _pageItem( 'Enero', 0 ),
+            _pageItem( 'Febrero', 1 ),
+            _pageItem( 'Marzo', 2 ),
+            _pageItem( 'Abril', 3 ),
+            _pageItem( 'Mayo', 4 ),
+            _pageItem( 'Junio', 5 ),
+            _pageItem( 'Julio', 6 ),
+            _pageItem( 'Agosto', 7 ),
+            _pageItem( 'Septiembre', 8 ),
+            _pageItem( 'Octubre', 9 ),
+            _pageItem( 'Noviembre', 10 ),
+            _pageItem( 'Diciembre', 11 ),
+          ]
+      ),
+    );
+  }
+  
+
+}
